ea13295fe6351bd7506bce0566e78356496931d9
wenkey-everest <muni.v.g@everest.engineering>
2022-12-19 21:56:04 +0530

    Add trigonometric functions tests

--- day_1/calculator_test.go
+++ day_1/calculator_test.go
@@ -17,16 +17,36 @@ func TestAdd(t *testing.T) {
 		}
 	})
 
-	t.Run("Should multiple two numbers", func(t *testing.T) {
+	t.Run("Should multiply two numbers", func(t *testing.T) {
 		result := mul(1, 2)
 		if result != 2 {
 			t.Error("result should be", result)
 		}
 	})
 
-	t.Run("Should division two numbers", func(t *testing.T) {
+	t.Run("Should divide two numbers", func(t *testing.T) {
 		result := div(4, 2)
 		if result != 2 {
+			t.Error("result should be", result)
+		}
+	})
+	t.Run("Should get value of sin", func(t *testing.T) {
+		result := sin(0.25)
+		if result != 0.004363309284746571 {
+			t.Error("result should be", result)
+		}
+	})
+
+	t.Run("Should get value of cos", func(t *testing.T) {
+		result := cos(0.25)
+		if result != 0.9999904807207345 {
+			t.Error("result should be", result)
+		}
+	})
+
+	t.Run("Should get value of tan", func(t *testing.T) {
+		result := tan(1)
+		if result != 0.017455064928217585 {
 			t.Error("result should be", result)
 		}
 	})
